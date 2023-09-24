800a509b78b57a81dbae96601a11b9361f4f9a32
Summy <Sunmibolaj13@gmail.com>
2023-09-23 21:59:45 -0400

    server commit

--- src/app.js
+++ src/app.js
@@ -1,30 +1,62 @@
 import express from "express";
 import cors from "cors";
+import http from "http";
+import { Server } from "socket.io";
 import morgan from "morgan";
-
 import * as middleware from "./utils/middleware.js";
 import helloRoute from "./routes/helloRouter.js";
 
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 
-// parse json request body
+// 1. Consider breaking these middleware and route definitions into separate modules for better organization.
+
+// Middleware
 app.use(express.json());
-
-// enable cors
 app.use(cors());
-
-// request logger middleware
 app.use(morgan("tiny"));
 
-// healthcheck endpoint
+// Healthcheck endpoint
 app.get("/", (req, res) => {
   res.status(200).send({ status: "ok" });
 });
 
+// Express routes
 app.use("/hello", helloRoute);
 
-// custom middleware
+// Custom middleware
 app.use(middleware.unknownEndpoint);
 app.use(middleware.errorHandler);
 
-export default app;
+// 2. Consider using environment variables for configuration settings such as the HTTP and WebSocket ports.
+const PORT = process.env.PORT || 3001;
+
+// Handle WebSocket connections
+io.on("connection", (socket) => {
+  console.log(`A user connected: ${socket.id}`);
+
+  // Handle incoming messages from clients
+  socket.on("message", (message) => {
+    // Broadcast the message to all connected clients (including the sender)
+    io.emit("message", message);
+  });
+
+  // Handle disconnections
+  socket.on("disconnect", () => {
+    console.log(`A user disconnected: ${socket.id}`);
+  });
+});
+
+// 3. Consider adding a try-catch block for error handling during server startup.
+try {
+  // Start the server
+  server.listen(PORT, () => {
+    console.log(`WebSocket server is running on port ${PORT}`);
+  });
+} catch (error) {
+  console.error("Server startup error:", error);
+}
+
+// Export the Express app and server if needed.
+export { app, server };
